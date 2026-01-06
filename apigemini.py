ab540168569443c57febe21bb98b6e5afce13926
Matteo <matteoscotti10@gmail.com>
2026-01-06 10:44:17 -0400

    Refatora a integração com a API Gemini para usar o cliente correto

--- apigemini.py
+++ apigemini.py
@@ -1,12 +1,13 @@
 import os
-import google.generativeai as genai
+import google as genai
 from dotenv import load_dotenv
 
 load_dotenv()
-genai.configure(api_key = os.getenv("GEMINI_API_KEY"))
+
+api_key = os.getenv("GEMINI_API_KEY")
 
 def resumir_transcricao_gemini(texto):
-    model = genai.GenerativeModel('gemini-1.5-flash')
+    client = genai.Client()
 
     prompt = f"""
     Você é um assistente especialista em resumos.
@@ -17,7 +18,10 @@ def resumir_transcricao_gemini(texto):
     {texto}
     """
     try:
-        response = model.generate_content(prompt)
+        response = client.models.generate_content(
+            model = 'gemini-1.5-flash',
+            content = prompt
+        )
         return response.text
     except Exception as e:
         return f"Erro ao gerar resumo: {e}"
